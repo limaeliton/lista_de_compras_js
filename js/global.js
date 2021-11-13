be182ebfe5edfa0aa796459096c0cc34e41b971c
Elitonlima2 <elitonlima2@hotmail.com>
2021-11-13 17:05:29 -0300

    criando a estrutura inicial dos arquivos

---
+++ js/global.js
@@ -0,0 +1,29 @@
+const cardNovalista = document.getElementById('cardNovalista')
+const btnformnovalista = document.getElementById('btnnovalista')
+const formnovalista = document.getElementById('novalista')
+const listas = []
+
+btnnovalista.addEventListener('click', function() {
+    cardNovalista.style.display = 'block'
+})
+
+formnovalista.addEventListener('submit', function(event) {
+    event.preventDefault()
+const inputtitulo = querySelector('input[name="tituloLista"]')
+if(inputtitulo.value !== "") {
+const novalista = {
+id: listas.length + 1,
+titulo: inputtitulo.value
+}
+inputtitulo.value = ''
+listas.push(novalista)
+window.localStorage.listas = json.stringify(listas)
+}
+console.log(listas)
+    
+})
+formnovalista.addEventListener('submit', function(event) {
+    cardNovalista.stlyle.display = "nome"
+    
+})
+
