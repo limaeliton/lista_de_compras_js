c1d8aa93a01ac22e8de099ef5ff510746563ec52
Elitonlima2 <elitonlima2@hotmail.com>
2021-11-15 09:48:06 -0300

    alteração no cardnovalista

--- js/global.js
+++ js/global.js
@@ -1,6 +1,7 @@
 const cardNovalista = document.getElementById('cardNovalista')
 const btnformnovalista = document.getElementById('btnnovalista')
 const formnovalista = document.getElementById('novalista')
+
 const listas = []
 
 btnnovalista.addEventListener('click', function() {
@@ -10,10 +11,10 @@ btnnovalista.addEventListener('click', function() {
 formnovalista.addEventListener('submit', function(event) {
     event.preventDefault()
 const inputtitulo = querySelector('input[name="tituloLista"]')
-if(inputtitulo.value !== "") {
+if(inputtitulo.value !== '') {
 const novalista = {
 id: listas.length + 1,
-titulo: inputtitulo.value
+titulo: inputtitulo.value 
 }
 inputtitulo.value = ''
 listas.push(novalista)
@@ -22,7 +23,7 @@ window.localStorage.listas = json.stringify(listas)
 console.log(listas)
     
 })
-formnovalista.addEventListener('submit', function(event) {
+formnovalista.addEventListener('reset', function(event) {
     cardNovalista.stlyle.display = "nome"
     
 })
