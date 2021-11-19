76d3b54b92f867420bfda7744bb207c9952ce79d
Elitonlima2 <elitonlima2@hotmail.com>
2021-11-18 22:01:27 -0300

    adicionar listas botoes

--- js/global.js
+++ js/global.js
@@ -1,8 +1,8 @@
 const cardNovalista = document.getElementById('cardNovalista')
 const btnformnovalista = document.getElementById('btnnovalista')
 const formnovalista = document.getElementById('novalista')
-
-const listas = []
+const divlistas = document.getElementById('listas')
+let listas = []
 
 btnnovalista.addEventListener('click', function() {
     cardNovalista.style.display = 'block'
@@ -10,7 +10,7 @@ btnnovalista.addEventListener('click', function() {
 
 formnovalista.addEventListener('submit', function(event) {
     event.preventDefault()
-const inputtitulo = querySelector('input[name="tituloLista"]')
+const inputtitulo = document.querySelector('input[name="tituloLista"]')
 if(inputtitulo.value !== '') {
 const novalista = {
 id: listas.length + 1,
@@ -18,13 +18,41 @@ titulo: inputtitulo.value
 }
 inputtitulo.value = ''
 listas.push(novalista)
-window.localStorage.listas = json.stringify(listas)
+window.localStorage.listas = JSON.stringify(listas)
 }
-console.log(listas)
-    
 })
 formnovalista.addEventListener('reset', function(event) {
     cardNovalista.stlyle.display = "nome"
-    
 })
+//função para adicionar os itens na tela
+function renderlist() {
+    // lendo as listas salvas no localStorage
+    listas = JSON.parse(window.localStorage.listas)
+    //limpando a div que recebe  os c
+    listas.map(lista =>{
+        //criando um titulo pro card
+    let h2tag = document.createElement('h2') 
+    // add o titulo da lista salva no título do card 
+    h2tag.innerHTML = lista.titulo
+    // cria btn
+   let btnaddItem = document.createElement('Button')
+   //adiciona texto ao btn
+
+
+   
+    let divtag = document.createElement('div')
+
+
+    // add a classe card-lista a div para o css
+    divtag.setAttribute('class', 'card-lista')
+    divtag.appendChild(h2tag)
+    divlistas.appendChild(divtag)
+    console.log(div)
+}
+
+if(window.localStorage.listas?.length > 0) {
+renderlist()
+
+})
+}
 
