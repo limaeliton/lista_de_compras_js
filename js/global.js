d59769f25ee88fba5b45922cf422911d95c8fc6c
Elitonlima2 <elitonlima2@hotmail.com>
2021-11-22 21:07:46 -0300

    alterações no js e css

--- js/global.js
+++ js/global.js
@@ -28,31 +28,31 @@ formnovalista.addEventListener('reset', function(event) {
 function renderlist() {
     // lendo as listas salvas no localStorage
     listas = JSON.parse(window.localStorage.listas)
-    //limpando a div que recebe  os c
+    //limpando a div que recebe  os cards da lista//
+    divlistas.innerHTML =""
+    //para cada lista arrey de listas, criar um novo card na tela//
     listas.map(lista =>{
         //criando um titulo pro card
     let h2tag = document.createElement('h2') 
-    // add o titulo da lista salva no título do card 
+    // adiciona o titulo da lista salva no título do card 
     h2tag.innerHTML = lista.titulo
-    // cria btn
+    // cria uma div para ser o card da lista//
    let btnaddItem = document.createElement('Button')
    //adiciona texto ao btn
 
+   let divtag = document.createElement('div')
+//adiciona a classe card-lista a div para o css//
 
-   
-    let divtag = document.createElement('div')
-
-
-    // add a classe card-lista a div para o css
     divtag.setAttribute('class', 'card-lista')
+// adiciona o h2 a div//
     divtag.appendChild(h2tag)
+    // adiciona a div .card-lista a div que contem todas a listas na tela//
     divlistas.appendChild(divtag)
     console.log(div)
 }
-
+//verificar se existe algo no localStorage para inicar//
 if(window.localStorage.listas?.length > 0) {
 renderlist()
 
 })
 }
-
